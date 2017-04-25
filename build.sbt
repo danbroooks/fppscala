danbroooks/herdingcats

Initial project structure

build.sbt
@@ -1,0 +1,7 @@
+name := "herdingcats"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+

src/main/scala/herdingcats/Main.scala
@@ -1,0 +1,5 @@
+package herdingcats
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}

danbroooks/fppscala

build.sbt
@@ -1,0 +1,7 @@
+name := "fppscala"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+

src/main/scala/fppscala/Main.scala
@@ -1,0 +1,5 @@
+package fppscala
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}