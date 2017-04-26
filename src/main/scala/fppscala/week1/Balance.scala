e1de45757f3ffb96304b0a8c5218605b8e50a902
Dan Brooks <dmbrooks@live.co.uk>
2017-04-26 09:23:58 +0100

    More tweaks to balance

--- src/main/scala/fppscala/week1/Balance.scala
+++ src/main/scala/fppscala/week1/Balance.scala
@@ -6,11 +6,10 @@ object Balance {
 
   def apply(chars: List[Char]): Boolean = {
     def f(c: List[Char], nOpenBraces: Int): Boolean = c match {
-      case _ if nOpenBraces < 0 => false
       case '(' :: tail => f(tail, nOpenBraces + 1)
-      case ')' :: tail => f(tail, nOpenBraces - 1)
-      case _ :: tail  => f(tail, nOpenBraces)
-      case _ => nOpenBraces == 0
+      case ')' :: tail => nOpenBraces > 0 && f(tail, nOpenBraces - 1)
+      case _ :: tail   => f(tail, nOpenBraces)
+      case _           => nOpenBraces == 0
     }
 
     f(chars, 0)
