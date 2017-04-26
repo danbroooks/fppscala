e3bef3e42e88117c58b1e1e7128821d68f333620
Dan Brooks <dmbrooks@live.co.uk>
2017-04-26 08:37:34 +0100

    Refactoring code for balance to use helper constructor

--- src/main/scala/fppscala/week1/Balance.scala
+++ src/main/scala/fppscala/week1/Balance.scala
@@ -1,6 +1,9 @@
 package fppscala.week1
 
 object Balance {
+  def apply(str: String): Boolean =
+    Balance(str.toList)
+
   def apply(chars: List[Char]): Boolean = {
     def f(c: List[Char], nOpenBraces: Int): Boolean = {
       if (c.isEmpty) true
--- src/test/scala/fppscala/week1/BalanceSpec.scala
+++ src/test/scala/fppscala/week1/BalanceSpec.scala
@@ -5,13 +5,13 @@ import org.scalatest._
 class BalanceSpec extends FreeSpec with Matchers {
   "Balance" - {
     "should return true when parens are balanced in string" in {
-      Balance("(if (zero? x) max (/ 1 x))".toList) should be(true)
-      Balance("I told him (that it's not (yet) done). (But he wasn't listening)".toList) should be(true)
+      Balance("(if (zero? x) max (/ 1 x))") should be(true)
+      Balance("I told him (that it's not (yet) done). (But he wasn't listening)") should be(true)
     }
 
     "should return false when parens aren't balanced in string" in {
-      Balance(":-)".toList) should be(false)
-      Balance("())(".toList) should be(false)
+      Balance(":-)") should be(false)
+      Balance("())(") should be(false)
     }
   }
 }
