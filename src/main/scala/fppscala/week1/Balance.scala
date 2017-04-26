1a6ffa96d73fc80c8d6422389cda9f8d01c9c046
Dan Brooks <dmbrooks@live.co.uk>
2017-04-26 09:21:09 +0100

    Write all logic in terms of match statement

--- src/main/scala/fppscala/week1/Balance.scala
+++ src/main/scala/fppscala/week1/Balance.scala
@@ -5,14 +5,12 @@ object Balance {
     Balance(str.toList)
 
   def apply(chars: List[Char]): Boolean = {
-    def f(c: List[Char], nOpenBraces: Int): Boolean = {
-      if (c.isEmpty) nOpenBraces == 0
-      else if (nOpenBraces < 0) false
-      else c.head match {
-        case '(' => f(c.tail, nOpenBraces + 1)
-        case ')' => f(c.tail, nOpenBraces - 1)
-        case _   => f(c.tail, nOpenBraces)
-      }
+    def f(c: List[Char], nOpenBraces: Int): Boolean = c match {
+      case _ if nOpenBraces < 0 => false
+      case '(' :: tail => f(tail, nOpenBraces + 1)
+      case ')' :: tail => f(tail, nOpenBraces - 1)
+      case _ :: tail  => f(tail, nOpenBraces)
+      case _ => nOpenBraces == 0
     }
 
     f(chars, 0)
